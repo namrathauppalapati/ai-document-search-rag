8d8971b25ee165ec7fae96b18a7f8e0235f1fe93
namrathauppalapati <namrathauppalapati@gmail.com>
2026-02-16 17:37:14 +0530

    Update text_loader.py

--- text_loader.py
+++ text_loader.py
@@ -1,4 +1,7 @@
 from langchain_community.document_loaders import TextLoader
-loader = TextLoader('cricket.txt')
-docs =loader.load()
+
+loader = TextLoader("data/notes.txt")
+docs = loader.load()
+
 print(len(docs))
+print(docs[0].page_content)
